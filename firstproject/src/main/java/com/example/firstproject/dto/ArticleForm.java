4d100fb030ff8d83415d69f8c827480ed11f7316
mnms2132 <guole1013@naver.com>
2023-11-24 16:17:19 +0900

    4일차-롬복과 리팩터링(Article생성자와toString매서드 어노테이션으로 교체)

--- firstproject/src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ firstproject/src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -1,24 +1,14 @@
 package com.example.firstproject.dto;
 
 import com.example.firstproject.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@ToString
 public class ArticleForm {
     private String title; //제목을 받을 필드
     private String content; //내용을 받을 필드
-
-    public ArticleForm(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
-
-    // 데이터를 잘 받았는지 확인할 toStirng()메서드 추가
-    @Override
-    public String toString() {
-        return "ArticleForm{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 
     public Article toEntity() {
         return new Article(null, title, content);
--- firstproject/src/main/java/com/example/firstproject/entity/Article.java
+++ firstproject/src/main/java/com/example/firstproject/entity/Article.java
@@ -4,7 +4,11 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor // Article()생성자 대체, 클래스 안쪽의 모든 필드를 매개변수로 하는 생성자 자동 생성
+@ToString // ToString() 매서드 대체
 @Entity
 public class Article {
     @Id //엔티티의 대푯값 지정
@@ -15,18 +19,4 @@ public class Article {
     @Column //content 필드 선언, DB 테이블의 content 열과 연결됨
     private String content;
 
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 }
