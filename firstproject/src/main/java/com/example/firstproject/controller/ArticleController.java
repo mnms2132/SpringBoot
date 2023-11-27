17362a8a7987e7712078345c8534e8a6d5b77001
HomePc <guole1013@naver.com>
2023-11-28 03:34:23 +0900

    7일차중간까지..

--- firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -68,4 +68,29 @@ public class ArticleController {
         //3.사용자에게 보여줄 뷰 페이지 설정하기
         return "articles/index";
     }
+
+    @GetMapping("/articles/{id}/edit")
+    public String edit(@PathVariable Long id, Model model){
+        //수정할 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        //모델에 데이터 등록하기
+        model.addAttribute("article", articleEntity);
+        //뷰페이지 설정하기
+        return "articles/edit";
+    }
+
+    @PostMapping("/articles/update")
+    public String update(ArticleForm form){
+        log.info(form.toString());
+        //1. DTO를 엔티티로 변환하기
+        Article articleEntity = form.toEntity();
+        log.info(articleEntity.toString());
+        //2. 엔티티를 DB에 저장하기
+        Article target = articleRepository.findById(articleEntity.getId()).orElse(null);
+        if (target !=null){
+            articleRepository.save(articleEntity);//엔티티를 DB에 저장(갱신)
+        }
+        //3. 수정 결과 페이지로 리다이렉트하기
+        return "redirect:/articles/"+articleEntity.getId();
+    }
 }
---
+++ firstproject/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO article(id, title, content) VALUE(1, 'AAAA', '1111');
+INSERT INTO article(id, title, content) VALUE(2, 'BBBB', '2222');
+INSERT INTO article(id, title, content) VALUE(3, 'CCCC', '3333');
