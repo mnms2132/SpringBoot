bc1a8e33e0d1226145d8d5e9abf584eaa175ef36
mnms2132 <guole1013@naver.com>
2023-11-30 03:13:31 +0900

    8일차-게시글삭제하기

--- firstproject/src/main/java/com/example/firstproject/controller/MemberController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/MemberController.java
@@ -1,6 +1,7 @@
 package com.example.firstproject.controller;
 
 import com.example.firstproject.dto.MemberForm;
+import com.example.firstproject.entity.Article;
 import com.example.firstproject.entity.Member;
 import com.example.firstproject.repository.MemberRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -52,6 +54,46 @@ public class MemberController {
         List<Member> MemberEntityList = memberRepository.findAll();
         model.addAttribute("memberList",MemberEntityList);
         return "members/index";
+    }
+
+    @GetMapping("/members/{id}/edit")
+    public String edit(@PathVariable Long id, Model model){
+        //1. 수정할 데이터 가져오기
+        Member memberEntity = memberRepository.findById(id).orElse(null);
+        //2. 모델에 데이터 등록하기
+        model.addAttribute("member", memberEntity);
+        //3. 뷰페이지 설정하기
+        return "members/edit";
+    }
+
+    @PostMapping("/members/update")
+    public String update (MemberForm form){
+        log.info(form.toString());
+        //1. DTO를 엔티티로 변환하기
+        Member memberEntity = form.toEntity();
+        log.info(memberEntity.toString());
+        //2. 엔티티를 DB에 저장하기
+        Member target = memberRepository.findById(memberEntity.getId()).orElse(null);
+        if (target != null) {
+            memberRepository.save(memberEntity); //DB저장 갱신
+        }
+        //3. 수정 결과 페이지로 리다이렉트하기
+        return "redirect:/members/"+memberEntity.getId();
+    }
+
+    @GetMapping("/members/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes rttr) {
+        log.info(id.toString());
+        log.info("삭제요청이들어왔습니다!");
+        //1. 데이터 가져오기
+        Member target = memberRepository.findById(id).orElse(null);
+        //2. 데이터 삭제하기
+        if (target != null) {
+            memberRepository.delete(target);
+            rttr.addFlashAttribute("msg","삭제됐습니다!");
+        }
+        //3. 결과페이지 반환하기
+        return "redirect:/members";
     }
 
 }
--- firstproject/src/main/resources/data.sql
+++ firstproject/src/main/resources/data.sql
@@ -1,3 +1,7 @@
 INSERT INTO article(id, title, content) VALUES(1, 'AAAA', '1111');
 INSERT INTO article(id, title, content) VALUES(2, 'BBBB', '2222');
 INSERT INTO article(id, title, content) VALUES(3, 'CCCC', '3333');
+
+INSERT INTO member(id, email, password) VALUES(1, 'mail@cloudstudying.kr', '1111');
+INSERT INTO member(id, email, password) VALUES(2, 'sehongpark@cloudstudying.kr', '2222');
+INSERT INTO member(id, email, password) VALUES(3, 'email@cloudstudying.kr', '3333');
